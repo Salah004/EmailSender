d3df12ff12c69e4b6024b48aaabf66fcacdce526
Salah004 <lachi.salah@outlook.fr>
2016-12-09 09:48:49 +0100

    Initial commit

---
+++ app/src/main/java/apps/salah/emailsender/Config.java
@@ -0,0 +1,9 @@
+package apps.salah.emailsender;
+
+/**
+ * Created by SALAH on 09/12/2016.
+ */
+public class Config {
+    public static final String EMAIL ="s_lachi@esi.dz";
+    public static final String PASSWORD ="xxxx";
+}
